b52c1c784c4674483e24617428c006c1fe4835e4
Lindsay Ridgeway <LDRidgeway@gmail.com>
2025-03-13 11:41:43 -0400

    Added authors to book list

--- bookSources.js
+++ bookSources.js
@@ -16,7 +16,7 @@ export const bookSources = [
     /*
   { 
     id: "montecristo", 
-    title: "The Count of Monte Cristo", 
+    title: "The Count of Monte Cristo by Alexandre Dumas",
     author: "Alexandre Dumas",
     url: "https://www.gutenberg.org/files/1184/1184-h/1184-h.htm#linkC2HCH0001",
     language: "en",
@@ -25,7 +25,7 @@ export const bookSources = [
     */
   { 
     id: "prideandprejudice", 
-    title: "Pride and Prejudice", 
+    title: "Pride and Prejudice by Jane Austen",
     author: "Jane Austen",
     url: "https://www.gutenberg.org/files/1342/1342-h/1342-h.htm#Chapter_I",
     language: "en",
@@ -33,7 +33,7 @@ export const bookSources = [
   },
   { 
     id: "sherlockholmes", 
-    title: "The Adventures of Sherlock Holmes", 
+    title: "The Adventures of Sherlock Holmes by Arthur Conan Doyle",
     author: "Arthur Conan Doyle",
     url: "https://www.gutenberg.org/files/48320/48320-h/48320-h.htm#i",
     language: "en",
@@ -42,7 +42,7 @@ export const bookSources = [
     /*
   { 
     id: "warandpeace", 
-    title: "War and Peace", 
+    title: "War and Peace by Leo Tolstoy",
     author: "Leo Tolstoy",
     url: "https://www.gutenberg.org/cache/epub/2600/pg2600-images.html#link2H_4_0001",
     language: "en",
@@ -51,7 +51,7 @@ export const bookSources = [
     */
   { 
     id: "aliceinwonderland", 
-    title: "Alice's Adventures in Wonderland", 
+    title: "Alice's Adventures in Wonderland by Lewis Carroll",
     author: "Lewis Carroll",
     url: "https://www.gutenberg.org/files/11/11-h/11-h.htm#chap01",
     language: "en",
@@ -59,7 +59,7 @@ export const bookSources = [
   },
   { 
     id: "mobydick", 
-    title: "Moby Dick", 
+    title: "Moby Dick by Herman Melville",
     author: "Herman Melville",
     url: "https://www.gutenberg.org/files/2701/2701-h/2701-h.htm#link2HCH0001",
     language: "en",
@@ -67,7 +67,7 @@ export const bookSources = [
     },
   { 
     id: "frankenstein", 
-    title: "Frankenstein", 
+    title: "Frankenstein by Mary Shelley",
     author: "Mary Shelley",
     url: "https://www.gutenberg.org/files/84/84-h/84-h.htm#letter1",
     language: "en",
@@ -75,7 +75,7 @@ export const bookSources = [
   },
   { 
     id: "donquixote", 
-    title: "Don Quixote", 
+    title: "Don Quixote by Miguel de Cervantes",
     author: "Miguel de Cervantes",
     url: "https://www.gutenberg.org/files/5921/5921-h/5921-h.htm#ch1",
     language: "en",
@@ -83,7 +83,7 @@ export const bookSources = [
     },
   { 
     id: "taleoftwocities", 
-    title: "A Tale of Two Cities", 
+    title: "A Tale of Two Cities by Charles Dickens",
     author: "Charles Dickens",
     url: "https://www.gutenberg.org/files/98/98-h/98-h.htm#link2H_4_0001",
     language: "en",
@@ -91,7 +91,7 @@ export const bookSources = [
   },
   { 
     id: "greatgatsby", 
-    title: "The Great Gatsby", 
+    title: "The Great Gatsby by F. Scott Fitzgerald",
     author: "F. Scott Fitzgerald",
     url: "https://www.gutenberg.org/files/64317/64317-h/64317-h.htm#chapter-1",
     language: "en",
@@ -99,7 +99,7 @@ export const bookSources = [
   },
   { 
     id: "dracula", 
-    title: "Dracula", 
+    title: "Dracula by Bram Stoker",
     author: "Bram Stoker",
     url: "https://www.gutenberg.org/files/345/345-h/345-h.htm#chap01",
     language: "en",
@@ -107,7 +107,7 @@ export const bookSources = [
   },
   { 
     id: "wizardofoz", 
-    title: "The Wonderful Wizard of Oz", 
+    title: "The Wonderful Wizard of Oz by L. Frank Baum",
     author: "L. Frank Baum",
     url: "https://www.gutenberg.org/files/55/55-h/55-h.htm#chap00",
     language: "en",
@@ -115,7 +115,7 @@ export const bookSources = [
   },
   { 
     id: "littlewomen", 
-    title: "Little Women", 
+    title: "Little Women by Louisa May Alcott",
     author: "Louisa May Alcott",
     url: "https://www.gutenberg.org/files/514/514-h/514-h.htm#part01",
     language: "en",
@@ -123,7 +123,7 @@ export const bookSources = [
     },
   { 
     id: "odyssey", 
-    title: "The Odyssey", 
+    title: "The Odyssey by Homer",
     author: "Homer",
     url: "https://www.gutenberg.org/files/1727/1727-h/1727-h.htm#chap01",
     language: "en",
@@ -131,7 +131,7 @@ export const bookSources = [
   },
   { 
     id: "iliad", 
-    title: "The Iliad", 
+    title: "The Iliad by Homer",
     author: "Homer",
     url: "https://www.gutenberg.org/files/2199/2199-h/2199-h.htm#chap01",
     language: "en",
@@ -139,7 +139,7 @@ export const bookSources = [
   },
   { 
     id: "treasureisland", 
-    title: "Treasure Island", 
+    title: "Treasure Island by Robert Louis Stevenson",
     author: "Robert Louis Stevenson",
     url: "https://www.gutenberg.org/files/120/120-h/120-h.htm#part01",
     language: "en",
@@ -147,7 +147,7 @@ export const bookSources = [
   },
   { 
     id: "tomsawyer", 
-    title: "The Adventures of Tom Sawyer", 
+    title: "The Adventures of Tom Sawyer by Mark Twain",
     author: "Mark Twain",
     url: "https://www.gutenberg.org/files/74/74-h/74-h.htm#c1",
     language: "en",
@@ -155,7 +155,7 @@ export const bookSources = [
   },
   { 
     id: "callofthewild", 
-    title: "The Call of the Wild", 
+    title: "The Call of the Wild by Jack London",
     author: "Jack London",
     url: "https://www.gutenberg.org/files/215/215-h/215-h.htm#chap01",
     language: "en",
@@ -164,7 +164,7 @@ export const bookSources = [
     /*
   { 
     id: "bible", 
-    title: "The Bible", 
+    title: "The Bible",
     author: "Traditional",
     url: "https://www.gutenberg.org/cache/epub/10/pg10-images.html#The_First_Book_of_Moses_Called_Genesis",
     language: "en",
@@ -173,7 +173,7 @@ export const bookSources = [
     */
     { 
     id: "republic", 
-    title: "The Republic", 
+    title: "The Republic by Plato",
     author: "Plato",
     url: "https://www.gutenberg.org/files/1497/1497-h/1497-h.htm#link2H_4_0002",
     language: "en",
@@ -181,7 +181,7 @@ export const bookSources = [
     },
   { 
     id: "zarathustra", 
-    title: "Thus Spoke Zarathustra", 
+    title: "Thus Spoke Zarathustra by Friedrich Nietzsche",
     author: "Friedrich Nietzsche",
     url: "https://www.gutenberg.org/files/1998/1998-h/1998-h.htm#link2H_4_0003",
     language: "en",
@@ -190,7 +190,7 @@ export const bookSources = [
     /*
   { 
     id: "lesmiserables", 
-    title: "Les Misérables", 
+    title: "Les Misérables by Victor Hugo",
     author: "Victor Hugo",
     url: "https://www.gutenberg.org/files/135/135-h/135-h.htm#link2H_PREF",
     language: "en",
@@ -199,7 +199,7 @@ export const bookSources = [
     */
   { 
     id: "ladywiththepet", 
-    title: "The Lady with the Dog", 
+    title: "The Lady with the Dog by Anton Chekhov",
     author: "Anton Chekhov",
     url: "https://www.gutenberg.org/files/13415/13415-h/13415-h.htm#THE_LADY_WITH_THE_DOG",
     language: "en",
@@ -207,7 +207,7 @@ export const bookSources = [
   },
   { 
     id: "littleprince", 
-    title: "The Little Prince", 
+    title: "The Little Prince by Antoine de Saint-Exupéry",
     author: "Antoine de Saint-Exupéry",
     url: "https://gutenberg.net.au/ebooks03/0300771h.html#ppded",
     language: "fr",
@@ -216,7 +216,7 @@ export const bookSources = [
     /*
   { 
     id: "annakarenina", 
-    title: "Anna Karenina", 
+    title: "Anna Karenina by Leo Tolstoy",
     author: "Leo Tolstoy",
     url: "https://www.gutenberg.org/files/1399/1399-h/1399-h.htm#chap01",
     language: "en",
@@ -225,7 +225,7 @@ export const bookSources = [
     */
   { 
     id: "dollshouse", 
-    title: "A Doll's House", 
+    title: "A Doll's House by Henrik Ibsen",
     author: "Henrik Ibsen",
     url: "https://www.gutenberg.org/files/2542/2542-h/2542-h.htm#act01",
     language: "en",
@@ -234,7 +234,7 @@ export const bookSources = [
     /*
   { 
     id: "crimeandpunishment", 
-    title: "Crime and Punishment", 
+    title: "Crime and Punishment by Fyodor Dostoyevsky",
     author: "Fyodor Dostoyevsky",
     url: "https://www.gutenberg.org/files/2554/2554-h/2554-h.htm#link2H_4_0002",
     language: "en",
@@ -243,7 +243,7 @@ export const bookSources = [
     */
   { 
     id: "christmascarol", 
-    title: "A Christmas Carol", 
+    title: "A Christmas Carol by Charles Dickens",
     author: "Charles Dickens",
     url: "https://www.gutenberg.org/files/19337/19337-h/19337-h.htm#Page_11",
     language: "en",
@@ -251,7 +251,7 @@ export const bookSources = [
   },
   { 
     id: "huckfinn", 
-    title: "The Adventures of Huckleberry Finn", 
+    title: "The Adventures of Huckleberry Finn by Mark Train",
     author: "Mark Train",
     url: "https://www.gutenberg.org/files/76/76-h/76-h.htm#chap01",
     language: "en",
