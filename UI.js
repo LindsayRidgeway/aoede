d96dad5b32e61dbeb45031fcdf95280cd50fa1e8
Lindsay Ridgeway <LDRidgeway@gmail.com>
2025-04-05 13:53:56 -0400

    Get logo from docs directory

--- UI.js
+++ UI.js
@@ -628,7 +628,7 @@ export function MainUI({
         <View style={styles.innerContainer}>
           <View style={styles.headerContainer}>
             <Image 
-              source={require('./assets/muse.png')} 
+              source={require('./docs/aoede_logo.png')} 
               style={styles.headerLogo} 
               resizeMode="contain"
             />
