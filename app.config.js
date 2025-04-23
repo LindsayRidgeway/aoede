808a4ceeb839317e596547756f46f7049f907f3a
Lindsay Ridgeway <LDRidgeway@gmail.com>
2025-04-23 09:05:49 -0400

    Increment iOS build number and update Android beta instructions

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ export default {
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false
       },
-      buildNumber: "103"
+      buildNumber: "104"
     },
     android: {
       adaptiveIcon: {
