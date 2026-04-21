88734e2f392efbb6b534679d54deddd2868b3a19
Lindsay Ridgeway <LDRidgeway@gmail.com>
2026-04-21 16:57:36 -0400

    Expand starter library to ten curated books

--- bookSources.js
+++ bookSources.js
@@ -20,10 +20,66 @@ export const bookSources = [
     format: "text"
   },
   {
+    id: "arsenelupin",
+    title: "Arsène Lupin, gentleman-cambrioleur by Maurice Leblanc",
+    author: "Maurice Leblanc",
+    url: "https://www.gutenberg.org/files/32854/32854-h/32854-h.htm#LARRESTATION_DARSENE_LUPIN",
+    language: "fr",
+    format: "text"
+  },
+  {
     id: "eightydays",
     title: "Around the World in Eighty Days by Jules Verne",
     author: "Jules Verne",
     url: "https://www.gutenberg.org/files/103/103-h/103-h.htm#chap01",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "prisonerofzenda",
+    title: "The Prisoner of Zenda by Anthony Hope",
+    author: "Anthony Hope",
+    url: "https://www.gutenberg.org/files/95/95-h/95-h.htm#chap01",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "scarletpimpernel",
+    title: "The Scarlet Pimpernel by Baroness Orczy",
+    author: "Baroness Orczy",
+    url: "https://www.gutenberg.org/files/60/60-h/60-h.htm#chap01",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "lostworld",
+    title: "The Lost World by Arthur Conan Doyle",
+    author: "Arthur Conan Doyle",
+    url: "https://www.gutenberg.org/files/139/139-h/139-h.htm#chap01",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "thirtyninesteps",
+    title: "The Thirty-Nine Steps by John Buchan",
+    author: "John Buchan",
+    url: "https://www.gutenberg.org/files/558/558-h/558-h.htm#chap01",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "kidnapped",
+    title: "Kidnapped by Robert Louis Stevenson",
+    author: "Robert Louis Stevenson",
+    url: "https://www.gutenberg.org/files/421/421-h/421-h.htm#link2HCH0001",
+    language: "en",
+    format: "text"
+  },
+  {
+    id: "bluecastle",
+    title: "The Blue Castle by L. M. Montgomery",
+    author: "L. M. Montgomery",
+    url: "https://www.gutenberg.org/files/67979/67979-h/67979-h.htm#CHAPTER_I",
     language: "en",
     format: "text"
   },
